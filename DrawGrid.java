c0f28bbb13ddfa52b744262ba987d0069dec2fd9
Shreshth Kharbanda <sk04us@gmail.com>
2019-09-17 10:26:30 -0700

    Add files via upload

--- DrawGrid.java
+++ DrawGrid.java
@@ -7,13 +7,25 @@ public class DrawGrid {
 	public static int col2 = col1 * 2;
 	public static int row1 = ySize / 3;
 	public static int row2 = row1 * 2;
-	public static Graphics g;
-	private static DrawingPanel panel;
+	private static DrawingPanel panel = new DrawingPanel(xSize, ySize);
+	public static Graphics g = getPanel().getGraphics();
+
+	public static final int SIZE = 150;
+	static String shape = "X";
+
+	static int square1X = DrawGrid.col1 / 2 - SIZE / 3;
+	static int square2X = (DrawGrid.col2 - DrawGrid.col1) / 2 + DrawGrid.col1 - SIZE / 3;
+	static int square3X = (panel.getWidth() - DrawGrid.col2) / 2 + DrawGrid.col2 - SIZE / 3;
+	static int row1Y = DrawGrid.row1 / 2 + SIZE / 3;
+	static int row2Y = (DrawGrid.row2 - DrawGrid.row1) / 2 + DrawGrid.row1 + SIZE / 3;
+	static int row3Y = (DrawGrid.row2 - DrawGrid.row1) / 2 + DrawGrid.row2 + SIZE / 3;
+
+	static Game game = new Game();
 
 	DrawGrid() {
-		setPanel(new DrawingPanel(xSize, ySize));
-		getPanel().setBackground(Color.WHITE);
-		g = getPanel().getGraphics();
+//		setPanel(new DrawingPanel(xSize, ySize));
+//		getPanel().setBackground(Color.WHITE);
+//		g = getPanel().getGraphics();
 
 		drawColumns();
 		drawRows();
@@ -30,10 +42,51 @@ public class DrawGrid {
 	}
 
 	public static DrawingPanel getPanel() {
-		return DrawGrid.panel;
+		return panel;
 	}
 
 	public void setPanel(DrawingPanel panel) {
 		DrawGrid.panel = panel;
+	}
+
+	public static void drawShape(int x, int y) {
+
+		if (Game.checkGrid() == false) {
+			g.setFont(new Font("Arial", Font.CENTER_BASELINE, SIZE));
+
+			if (x < DrawGrid.col1 && y < DrawGrid.row1 && !Game.getMap().get(1).equals(shape)) {
+				g.drawString(shape, square1X, row1Y);
+				Game.getMap().put(1, shape);
+			} else if ((x > DrawGrid.col1 && x < DrawGrid.col2) && y < DrawGrid.row1) {
+				g.drawString(shape, square2X, row1Y);
+				Game.getMap().put(2, shape);
+			} else if (x > DrawGrid.col2 && y < DrawGrid.row1) {
+				g.drawString(shape, square3X, row1Y);
+				Game.getMap().put(3, shape);
+			}
+
+			if (x < DrawGrid.col1 && (y < DrawGrid.row2 && y > DrawGrid.row1)) {
+				g.drawString(shape, square1X, row2Y);
+				Game.getMap().put(4, shape);
+			} else if ((x > DrawGrid.col1 && x < DrawGrid.col2) && (y < DrawGrid.row2 && y > DrawGrid.row1)) {
+				g.drawString(shape, square2X, row2Y);
+				Game.getMap().put(5, shape);
+			} else if (x > DrawGrid.col2 && (y < DrawGrid.row2 && y > DrawGrid.row1)) {
+				g.drawString(shape, square3X, row2Y);
+				Game.getMap().put(6, shape);
+			}
+
+			if (x < DrawGrid.col1 && y > DrawGrid.row2) {
+				g.drawString(shape, square1X, row3Y);
+				Game.getMap().put(7, shape);
+			} else if ((x > DrawGrid.col1 && x < DrawGrid.col2) && y > DrawGrid.row2) {
+				g.drawString(shape, square2X, row3Y);
+				Game.getMap().put(8, shape);
+			} else if (x > DrawGrid.col2 && y > DrawGrid.row2) {
+				g.drawString(shape, square3X, row3Y);
+				Game.getMap().put(9, shape);
+			}
+			shape = shape == "X" ? "O" : "X";
+		}
 	}
 }
--- Game.java
+++ Game.java
@@ -1,41 +1,34 @@
 import java.awt.*;
+import java.util.*;
 
 public class Game {
-	static DrawGrid d;
-	static Graphics g;
-	static DrawingPanel panel;
-	public static final int SIZE = 150;
+	static DrawGrid d = new DrawGrid();
+	static DrawingPanel panel = DrawGrid.getPanel();
+	static Graphics g = panel.getGraphics();
+
+	static HashMap<Integer, String> map = new HashMap<Integer, String>();
 
 	public static void main(String[] args) {
-		d = new DrawGrid();
-		panel = DrawGrid.getPanel();
-		g = panel.getGraphics();
-		
-		panel.onClick((x, y) -> drawShape(x, y));
+		panel.onClick((x, y) -> DrawGrid.drawShape(x, y));
 	}
 
-	private static void drawShape(int x, int y) {
-		g.setFont(new Font("Arial", Font.CENTER_BASELINE, SIZE));
-		
-		if(x < DrawGrid.col1 && y < DrawGrid.row1)
-			g.drawString("X", DrawGrid.col1/2 - SIZE/3, DrawGrid.row1/2 + SIZE/3);
-		if((x > DrawGrid.col1 && x < DrawGrid.col2) && y < DrawGrid.row1)
-			g.drawString("X", (DrawGrid.col2 - DrawGrid.col1)/2 + DrawGrid.col1 - SIZE/3, DrawGrid.row1/2 + SIZE/3);
-		if(x > DrawGrid.col2 && y < DrawGrid.row1)
-			g.drawString("X", (panel.getWidth() - DrawGrid.col2)/2 + DrawGrid.col2 - SIZE/3, DrawGrid.row1/2 + SIZE/3);
-		
-		if(x < DrawGrid.col1 && (y < DrawGrid.row2 && y > DrawGrid.row1))
-				g.drawString("X", DrawGrid.col1/2 - SIZE/3, (DrawGrid.row2 - DrawGrid.row1)/2 + DrawGrid.row1 + SIZE/3);
-		if((x > DrawGrid.col1 && x < DrawGrid.col2) && (y < DrawGrid.row2 && y > DrawGrid.row1))
-			g.drawString("X", (DrawGrid.col2 - DrawGrid.col1)/2 + DrawGrid.col1 - SIZE/3, (DrawGrid.row2 - DrawGrid.row1)/2 + DrawGrid.row1 + SIZE/3);
-		if(x > DrawGrid.col2 && (y < DrawGrid.row2 && y > DrawGrid.row1))
-			g.drawString("X", (panel.getWidth() - DrawGrid.col2)/2 + DrawGrid.col2 - SIZE/3, (DrawGrid.row2 - DrawGrid.row1)/2 + DrawGrid.row1 + SIZE/3);
-		
-		if(x < DrawGrid.col1 && y > DrawGrid.row2)
-			g.drawString("X", DrawGrid.col1/2 - SIZE/3, (DrawGrid.row2 - DrawGrid.row1)/2 + DrawGrid.row2 + SIZE/3);
-		if((x > DrawGrid.col1 && x < DrawGrid.col2) && y > DrawGrid.row2)
-			g.drawString("X", (DrawGrid.col2 - DrawGrid.col1)/2 + DrawGrid.col1 - SIZE/3, (DrawGrid.row2 - DrawGrid.row1)/2 + DrawGrid.row2 + SIZE/3);
-		if(x > DrawGrid.col2 && y > DrawGrid.row2)
-			g.drawString("X", (panel.getWidth() - DrawGrid.col2)/2 + DrawGrid.col2 - SIZE/3, (DrawGrid.row2 - DrawGrid.row1)/2 + DrawGrid.row2 + SIZE/3);
+	public static HashMap<Integer, String> getMap() {
+		return map;
 	}
+	
+	public static boolean checkGrid() {
+		try {
+			
+			
+			if (map.get(1).equals(map.get(2)) && map.get(2).equals(map.get(3))) {
+				System.out.println(map.get(1) + " WINS!");
+				return true;
+			}
+			return false;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+	}
+
 }

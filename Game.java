084acd690b73ec5fdc8da8b92c31a51e446f8a9f
Shantanu Singh <47289250+Cyphion@users.noreply.github.com>
2019-09-22 17:35:02 -0700

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,15 +1,14 @@
-import java.awt.*;
-import java.util.*;
-
 public class Game {
-	static DrawGrid d = new DrawGrid();
-	static DrawingPanel panel = DrawGrid.getPanel();
-	static Graphics g = panel.getGraphics();
 
-	static int[] map = new int[9];
+	public static DrawGrid d;
+	public static DrawingPanel panel;
+	public static String winner;
+
+	static int[] map;
+	static String[] shapes = {"X", "O"};
 
 	public static void main(String[] args) {
-		panel.onClick((x, y) -> DrawGrid.drawShape(x, y));
+		startGame();
 	}
 
 	public static int[] getMap() {
@@ -26,35 +25,73 @@ public class Game {
 	}
 
 	public static boolean winCombos() {
-		System.out.println(Arrays.toString(map));
-		// Find way to continue checking for win combos even if some values are null
-		// Only issue with the actual game
-		if ((map[0] != 0) && (map[0] == (map[1]) && map[1] == (map[2]))) {
-			System.out.println(map[0] + " WINS!");
+		if(checkRows() || checkCols() || checkVert()){
+			d.playAgain(winner  + " Wins");
 			return true;
-		} else if ((map[3] != 0) && map[3] == (map[4]) && map[4] == (map[5])) {
-			System.out.println(map[3] + " WINS!");
-			return true;
-		} else if ((map[6] != 0) && map[6] == (map[7]) && map[7] == (map[8])) {
-			System.out.println(map[6] + " WINS!");
-			return true;
-		} else if ((map[0] != 0) && map[0] == (map[3]) && map[3] == (map[6])) {
-			System.out.println(map[0] + " WINS!");
-			return true;
-		} else if ((map[1] != 0) && map[1] == (map[4]) && map[4] == (map[7])) {
-			System.out.println(map[0] + " WINS!");
-			return true;
-		} else if ((map[2] != 0) && map[2] == (map[5]) && map[5] == (map[8])) {
-			System.out.println(map[0] + " WINS!");
-			return true;
-		} else if ((map[0] != 0) && map[0] == (map[4]) && map[4] == (map[8])) {
-			System.out.println(map[0] + " WINS!");
-			return true;
-		} else if ((map[2] != 0) && map[2] == (map[4]) && map[4] == (map[6])) {
-			System.out.println(map[0] + " WINS!");
+		}else if(checkDraw()){
+			d.playAgain("It's a Draw");
 			return true;
 		}
 		return false;
+	}
+
+	private static boolean checkRows(){
+		if ((map[0] != 0) && (map[0] == (map[1]) && map[1] == (map[2]))) {
+			winner = shapes[map[0]-1];
+			return true;
+		} else if ((map[3] != 0) && map[3] == (map[4]) && map[4] == (map[5])) {
+			winner = shapes[map[0]-1];
+			return true;
+		} else if ((map[6] != 0) && map[6] == (map[7]) && map[7] == (map[8])) {
+			winner = shapes[map[0]-1];
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean checkCols(){
+		if ((map[0] != 0) && map[0] == (map[3]) && map[3] == (map[6])) {
+			winner = shapes[map[0]-1];
+			return true;
+		} else if ((map[1] != 0) && map[1] == (map[4]) && map[4] == (map[7])) {
+			winner = shapes[map[0]-1];
+			return true;
+		} else if ((map[2] != 0) && map[2] == (map[5]) && map[5] == (map[8])) {
+			winner = shapes[map[0]-1];
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean checkVert(){
+		if ((map[0] != 0) && map[0] == (map[4]) && map[4] == (map[8])) {
+			winner = shapes[map[0]-1];
+			return true;
+		} else if ((map[2] != 0) && map[2] == (map[4]) && map[4] == (map[6])) {
+			winner = shapes[map[0]-1];
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean checkDraw(){
+		for(int i = 0; i < map.length;i++){
+			if(map[i] == 0){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void startGame(){
+		map = new int[9];
+		d = new DrawGrid();
+		panel = d.getPanel();
+		panel.onClick((x, y) -> d.drawShape(x, y));
+	}
+
+	public static void closePrev() {
+		panel.setVisible(false);
 	}
 
 }
